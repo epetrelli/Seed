905126ef2f1cb02fcc46ee884f36ce952a66abbb
Enrico Petrelli <enrico.petrelli@wirge.com>
2015-11-19 20:24:53 +0100

    with app works...

--- src/main/java/it/wirge/rest/SeedApplication.java
+++ src/main/java/it/wirge/rest/SeedApplication.java
@@ -1,8 +1,18 @@
 package it.wirge.rest;
 
+import org.glassfish.jersey.server.ResourceConfig;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("api")
-public class SeedApplication extends Application {
+public class SeedApplication extends ResourceConfig {
+
+    public SeedApplication(){
+
+        register(new SeedApplicationBinder());
+        packages("it.wirge.rest.endpoints");
+
+    }
+
 }
--- src/main/java/it/wirge/rest/endpoints/Heartbeat.java
+++ src/main/java/it/wirge/rest/endpoints/Heartbeat.java
@@ -1,11 +1,15 @@
 package it.wirge.rest.endpoints;
 
+import it.wirge.data.dao.SeedDao;
+import it.wirge.data.entites.Seed;
 import it.wirge.rest.models.Beat;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 import java.util.logging.Logger;
 
 
@@ -14,12 +18,21 @@ public class Heartbeat {
 
     Logger logger = Logger.getLogger(this.getClass().getName());
 
+    @Inject
+    private SeedDao seedDao;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Beat getHeartbeat() {
         logger.info("getHeartbeat()");
         Beat beat = new Beat();
         beat.setCurrentTime(System.currentTimeMillis());
+
+        List<Seed> seeds = seedDao.getSeeds();
+
+        logger.info(seeds.size() + " seeds");
+
+
         return beat;
     }
 }
