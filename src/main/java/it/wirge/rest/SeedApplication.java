5e2079cb8796ef9627ad61d5669a4d9c1936aad2
Enrico Petrelli <enrico.petrelli@wirge.com>
2015-11-19 12:20:13 +0100

    with app works...

---
+++ src/main/java/it/wirge/rest/SeedApplication.java
@@ -0,0 +1,12 @@
+package it.wirge.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by enricopetrelli on 19/11/15.
+ */
+
+@ApplicationPath("api")
+public class SeedApplication extends Application {
+}
