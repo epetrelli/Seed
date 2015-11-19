39bf6a2ef39ed64593fc4ae1c37c7119bd68e4f8
Enrico Petrelli <enrico.petrelli@wirge.com>
2015-11-19 18:52:49 +0100

    with app works...

--- src/main/java/it/wirge/rest/endpoints/Heartbeat.java
+++ src/main/java/it/wirge/rest/endpoints/Heartbeat.java
@@ -6,14 +6,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.logging.Logger;
 
 
 @Path("/heartbeat")
 public class Heartbeat {
 
+    Logger logger = Logger.getLogger(this.getClass().getName());
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Beat getHeartbeat() {
+        logger.info("getHeartbeat()");
         Beat beat = new Beat();
         beat.setCurrentTime(System.currentTimeMillis());
         return beat;
