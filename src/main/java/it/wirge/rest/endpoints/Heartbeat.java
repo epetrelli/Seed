63cff393ff7664721e77cb86afa7fad46bf15375
epetrelli <enrico.petrelli@wirge.it>
2015-11-19 02:56:47 +0100

    Initial commit

---
+++ src/main/java/it/wirge/rest/endpoints/Heartbeat.java
@@ -0,0 +1,23 @@
+package it.wirge.rest.endpoints;
+
+import it.wirge.rest.models.Beat;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by enrico on 19/11/15.
+ */
+@Path("/heartbeat")
+public class Heartbeat {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Beat getHeartbeat() {
+        Beat beat = new Beat();
+        beat.setCurrentTime(System.currentTimeMillis());
+        return beat;
+    }
+}
---
+++ src/main/java/it/wirge/rest/models/Beat.java
@@ -0,0 +1,17 @@
+package it.wirge.rest.models;
+
+/**
+ * Created by enrico on 19/11/15.
+ */
+public class Beat {
+
+    private Long currentTime;
+
+    public Long getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(Long currentTime) {
+        this.currentTime = currentTime;
+    }
+}
